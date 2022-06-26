83e29b1685ad6b24be0459f3211ae1b70d1c89df
JustaMushroom <1234madguy@gmail.com>
2022-06-26 22:33:21 +0100

    Extend the teams control functionality

--- src/main/java/com/justamushroom/yetanothercompasstracker/utility/Teams.java
+++ src/main/java/com/justamushroom/yetanothercompasstracker/utility/Teams.java
@@ -6,6 +6,8 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Team;
 
+import javax.annotation.Nullable;
+
 public class Teams {
 
     // Overload of changePlayer team to make silent a non-required parameter
@@ -15,17 +17,49 @@ public class Teams {
 
     //TODO: Add more error handling
     public static void changePlayerTeam(Player plr, Team targetTeam, boolean silent) {
-        Team currentTeam = Bukkit.getScoreboardManager().getMainScoreboard().getEntryTeam(plr.getName()); // Get player's current team (if applicable)
+        // Get player's current team (if applicable)
+        Team currentTeam = Bukkit.getScoreboardManager().getMainScoreboard().getEntryTeam(plr.getName());
 
         if (currentTeam != null) {
-            currentTeam.removeEntry(plr.getName()); // Remove player from current team
+            currentTeam.removeEntry(plr.getName()); // Remove player from their current team
         }
 
         targetTeam.addEntry(plr.getName()); // Add player to new team
 
         // Broadcast team change in chat if not done silently
         if (!silent) {
-            Bukkit.getServer().broadcastMessage(plr.getDisplayName() + " is now on team " + ChatColor.BOLD + targetTeam.getColor() + targetTeam.getName() + ChatColor.RESET);
+            Bukkit.getServer().broadcastMessage(plr.getDisplayName() + " is now on team "
+                    + ChatColor.BOLD + targetTeam.getColor() + targetTeam.getName() + ChatColor.RESET);
+        }
+    }
+
+    public static void removePlayerTeam(Player plr) {
+        removePlayerTeam(plr, false);
+    }
+
+    // Removes a player from their team (if they are in one)
+    public static void removePlayerTeam(Player plr, boolean silent) {
+        // Get player's current team
+        Team currentTeam = Bukkit.getScoreboardManager().getMainScoreboard().getEntryTeam(plr.getName());
+
+        if (currentTeam != null) {
+            // Remove the player from their team
+            currentTeam.removeEntry(plr.getName());
+
+            // Broadcast the removal message to all players if not done silently
+            if (!silent) {
+                Bukkit.getServer().broadcastMessage(plr.getDisplayName() + " is no longer on a team!");
+            }
+        }
+    }
+
+    // Get the Team object of a player's team. Returns null if they are not in a team
+    @Nullable
+    public static Team getPlayerTeam(Player plr) {
+        try {
+            return Bukkit.getScoreboardManager().getMainScoreboard().getEntryTeam(plr.getName());
+        } catch (NullPointerException error) {
+            return null;
         }
     }
 }
